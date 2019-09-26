f380e1bdd364101669ec732241a316cc16ecee0e
Stephen Thompson <neebs@live.ca>
2019-09-25 18:09:38 -0700

    added addArtist function

--- script_lab3.js
+++ script_lab3.js
@@ -9,20 +9,44 @@ function toggleForm(a) {
   }
 }
 
-function addArtist() {
+function getInput(a) {
+  var input = document.getElementById(a);  
+  var vals;  
+  var i;
+
+  for (i = 0; i < input.length; i++) {
+    vals.push(input.elements[i].value);
+  }
+
+  return vals;
+}
+
+function addArtist(a) {
+  var vals = getInput(a);
+
   body = document.getElementsByTagName('body');  
 
   artistsDiv = document.createElement('div');
   artistsDiv.id = 'artists-div';  
 
   artistImage = document.createElement('img');
-  
+  artistImage.setAttribute("src", vals.elements[2].value);
+  artistImage.setAttribute("alt", "artist"); 
+
   artistsTextDiv = document.createElement('div');
   artistsTextDiv.id = 'artists-text-div';  
 
   artistName = document.createElement('span');
   artistName.id = 'artist-name';
+  var artistNameTextNode = document.createTextNode(vals.elements[0].value);
 
   artistDesc = document.createElement('span');
   artistDesc.id = 'artist-desc';
+  var artistDescTextNode = document.createTextNode(vals.elements[1].value);
+
+  body.appendChild(artistsDiv);
+  artistsDiv.appendChild(artistsImage);
+  artistsDiv.appendChild(artistsTextDiv);
+  artistsTextDiv.appendChild(artistName);
+  artistsTextDiv.appendChild(artistDesc);
 }
